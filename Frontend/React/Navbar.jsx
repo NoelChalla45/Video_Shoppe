0cd80064433c64c754700b08ff78cc2f600824b7
Deondre' Mayo <deondresm@gmail.com>
2026-02-18 21:23:34 -0500

    Navbar.jsx

--- Frontend/React/Navbar.jsx
+++ Frontend/React/Navbar.jsx
@@ -1,15 +1,17 @@
 /*Idea: On account page, have a log out button that sends you back to the login screen*/
 import "../styles/navbar.css";
+import { Link } from "react-router-dom";
+
 
 export default function Navbar() {
     return(
         <nav className="navbar">
             <h2 className="logo">Video Shoppe</h2>
             <ul className="nav-links">
-                <li><a href="#home">Home</a></li>
-                <li><a href="#catalog">DVD Catalog</a></li>
-                <li><a href="#alerts">Rental Alerts</a></li>
-                <li><a href="#account">Account</a></li>
+                <li><Link to="/home">Home</Link></li>
+                <li><Link to="/catalog">DVD Catalog</Link></li>
+                <li><Link to="/alerts">Rental Alerts</Link></li>
+                <li><Link to="/account">Account</Link></li>
             </ul> 
 
            {/*<button className="login-btn">Login</button>*/}
