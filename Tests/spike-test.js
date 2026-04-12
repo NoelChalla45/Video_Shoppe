2f66a3d3f100b3c23fa07b3fd2b8164f49baf54b
Deondre' Mayo <deondresm@gmail.com>
2026-04-12 15:54:20 -0400

    Update spike-test.js

--- Tests/spike-test.js
+++ Tests/spike-test.js
@@ -3,15 +3,15 @@ import { sleep } from 'k6';
 
 export const options = {
   stages: [
-    { duration: '10s', target: 100 }, // Normal load
+    { duration: '10s', target: 100 }, 
     { duration: '1m', target: 100 },
-    { duration: '10s', target: 1400 }, // SPICE: High load
-    { duration: '3m', target: 1400 }, // Stay at high load
-    { duration: '10s', target: 0 }, // Rapid Scale down
+    { duration: '10s', target: 1400 }, 
+    { duration: '3m', target: 1400 }, 
+    { duration: '10s', target: 0 },
   ],
 };
 
 export default function () {
-  http.get('http://localhost:5173');  // home page URL
+  http.get('http://localhost:5173');  
   sleep(1);
 }
